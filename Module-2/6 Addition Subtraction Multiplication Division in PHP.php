21b7fbe8566b51a0174aa45845ac02ba84d1b50b
Efthakhar <efthakharbin@gmail.com>
2023-01-31 23:57:29 +0600

    module2 finished upto 7

---
+++ Module-2/6 Addition Subtraction Multiplication Division in PHP.php
@@ -0,0 +1,29 @@
+<?php
+
+
+$number = 12;
+
+// adding numbers in php
+$number = $number + 6;
+// another way of addition
+$number += 6; // this means $number = $number + 6
+echo $number;
+
+// division in php
+$result = 15/5;
+$result = 15;
+$result /= 5; // result = result/5
+
+echo $result;
+
+// product in php
+$result = $result*2;
+echo $result;
+
+// arithmetic operator
+// +,-,*,/
+
+
+
+
+?>

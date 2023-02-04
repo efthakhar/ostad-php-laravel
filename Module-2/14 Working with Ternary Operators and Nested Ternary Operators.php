09e839d967b3f4b6984ff38ebbce3572858b2644
Efthakhar <efthakharbin@gmail.com>
2023-02-04 21:55:39 +0600

    video 15 completed

---
+++ Module-2/14 Working with Ternary Operators and Nested Ternary Operators.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$n = 12;
+
+$result = ($n==10) ? 'ten' : ($n==15 ? 'fifteen':($n==12?'twelve':'not any number'));
+echo $result;
+echo "\n";
+
+
+?>
---
+++ Module-2/15 swich statement.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$color = 'blue';
+
+switch($color){
+    case 'red':
+    echo "color is red";
+    break;
+    case 'green':
+    echo 'color is green';
+    break;
+    case 'gr':
+    case 'blue':
+    echo 'not my favourite';
+    break;
+    default:
+    echo 'no color matched';
+}
+
+    
+
+
+?>

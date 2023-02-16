28e123526d434b4b5dd9ef0e114432c41d8c187c
Efthakhar <efthakharbin@gmail.com>
2023-02-16 19:31:32 +0600

    Module-4 lesson-9 isset and empty function

---
+++ Module-4/9. Discussion of Empty Values ​​.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+
+// isset() return true if the variable is set or it is not null
+// case 1: $var; // return false
+// case 2: $var = null; // return false
+// other cases: return true
+
+$var;
+//echo isset($var); 
+
+//On the other hand the empty() function checks if the variable has an empty value empty string, 0, NULL or False. 
+
+$var2 = ''; 
+
+echo empty($var2); 
+// return true for: empty string,
+//                  NULL
+//                  False           
+//                  0
+
+
+?>
